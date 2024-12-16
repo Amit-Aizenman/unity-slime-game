f2f21b1b69bc64aed37085d80910fb962e7b7ecf
amitaizenman <amit.aizenman1@mail.huji.ac.il>
2024-12-16 22:33:07 +0200

    finished player animation logic and added enemy object

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,24 +1,27 @@
 using System;
+using TreeEditor;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    [SerializeField] int maxSpeed = 100;
+    [SerializeField] int maxSpeed = 5;
     [SerializeField] private float force = 1000;
     private Rigidbody2D _rigidbody2D;
     private float _horizontalMovement;
     private float _verticalMovement;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Animator animator;
+    
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         _horizontalMovement = Input.GetAxis("Horizontal");
         _verticalMovement = Input.GetAxis("Vertical");
+        animator.SetFloat("speed", _rigidbody2D.linearVelocity.magnitude);
     }
 
     private void FixedUpdate()
@@ -27,6 +30,14 @@ public class CharacterMovement : MonoBehaviour
         {
             _rigidbody2D.AddForce(Vector2.right * (_horizontalMovement * Time.fixedDeltaTime * force));
             _rigidbody2D.AddForce(Vector2.up * (_verticalMovement * Time.fixedDeltaTime * force));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            
         }
     }
 }
--- Assets/Scripts/PropMovementTweener.cs
+++ Assets/Scripts/PropMovementTweener.cs
@@ -5,7 +5,6 @@ public class PropMovementTweener : MonoBehaviour
 {
     private Vector3 startScale;
     [SerializeField] float delay;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startScale = transform.localScale;
@@ -14,8 +13,7 @@ public class PropMovementTweener : MonoBehaviour
         transform.DOScale(startScale, 1.5f).SetDelay(delay);
         transform.DORotate(new Vector3(0,0,1080f),1.5f, RotateMode.FastBeyond360).SetDelay(delay);
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         

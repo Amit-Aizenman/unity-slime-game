fb42c585d87ac0f55d876ee69da05e79ba518590
amitaizenman <amit.aizenman1@mail.huji.ac.il>
2024-12-17 14:08:42 +0200

    changed folders of animation and finished enemy animation logic

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,6 +5,7 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private int damage = 30;
     [SerializeField] private int hitsToDeath = 2;
+    private Animator animator;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player") {
